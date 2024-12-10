198b507d0955ff6f589ae0e71d20f99cd223e005
dotthor(home-ws) <dotthor.dev@gmail.com>
2024-12-10 23:51:30 +0100

    pnpm update

---
+++ src/routes/dnd/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import {
+		draggable,
+		dropTargetForElements
+	} from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
+
+	let ref = null;
+	let ref2 = null;
+	onMount(() => {
+		dropTargetForElements({ element: ref2 });
+		return draggable({ element: ref });
+	});
+</script>
+
+<div class="size-96 bg-red-400" bind:this={ref2}>
+	<div class="size-40 bg-blue-400" bind:this={ref}></div>
+</div>
