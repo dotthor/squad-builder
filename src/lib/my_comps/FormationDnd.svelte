167dcb4617dea97d65035e4e632a4020f555e0a7
dotthor(home-ws) <dotthor.dev@gmail.com>
2024-12-13 00:25:58 +0100

    campetto chill

--- src/lib/my_comps/FormationDnd.svelte
+++ src/lib/my_comps/FormationDnd.svelte
@@ -9,9 +9,51 @@
 </script>
 
 <div
-	class="borderinn grid aspect-[9/16] w-[80%] grid-cols-5 grid-rows-[repeat(15,_minmax(0,_1fr))] border border-blue-500 bg-teal-200 sm:w-96"
+	class="grid aspect-[68/105] h-auto w-[95%] grid-cols-5 grid-rows-[repeat(15,_minmax(0,_1fr))] bg-green-300 p-4 sm:h-[95%] sm:w-auto"
 >
-	<div class="col-start-3 row-start-6 flex size-full items-center justify-center">
+	<div class="col-span-full col-start-1 row-start-1 flex size-full">
+		<span class="h-2 w-full bg-white sm:h-4"></span>
+	</div>
+	<div class="col-span-full col-start-1 row-start-[15] flex size-full items-end">
+		<span class="h-2 w-full bg-white sm:h-4"></span>
+	</div>
+	<div class="col-start-1 row-span-full row-start-1 flex size-full items-end">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-start-5 row-span-full row-start-1 flex size-full justify-end">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-start-2 row-span-2 row-start-1 flex size-full justify-start">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-start-4 row-span-2 row-start-1 flex size-full justify-end">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-span-3 col-start-2 row-start-3 flex size-full">
+		<span class="h-2 w-full bg-white sm:h-4"></span>
+	</div>
+	<div class="col-span-full col-start-1 row-start-8 flex size-full items-center">
+		<span class="h-2 w-full bg-white sm:h-4"></span>
+	</div>
+	<div class="col-start-3 row-span-3 row-start-7 flex size-full items-center justify-center px-1">
+		<span class="aspect-square w-full rounded-full border-8 border-white sm:border-[16px]"></span>
+	</div>
+	<div class="col-start-2 row-span-2 row-start-[14] flex size-full justify-start">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-start-4 row-span-2 row-start-[14] flex size-full justify-end">
+		<span class="h-full w-2 bg-white sm:w-4"></span>
+	</div>
+	<div class="col-span-3 col-start-2 row-start-[13] flex size-full items-end">
+		<span class="h-2 w-full bg-white sm:h-4"></span>
+	</div>
+	<div class="col-start-3 row-span-2 row-start-11 flex size-full items-center justify-center">
+		<span class="h-[90%] w-[85%] rounded-full bg-slate-600"></span>
+	</div>
+	<!--  -->
+	<!--  -->
+	<!--  -->
+	<div class="col-start-3 row-start-1 flex size-full items-end justify-center">
 		<svg class="size-6" viewBox="0 0 40 40" preserveAspectRatio="none" width="100%" height="100%"
 			><rect
 				fill="#357edd"
